6be4a16ac8ae2875cecfc8d1499db25ca46cecf1
flippedround <734243792@qq.com>
2025-05-25 14:26:53 +0800

    fix: code load

--- components/content/CodeSnippet.vue
+++ components/content/CodeSnippet.vue
@@ -7,17 +7,19 @@ const { url, language, filename } = defineProps<{
 
 const loadedCode = ref('')
 
-try {
-  const data = await $fetch<string>(
-    `https://raw.githubusercontent.com/crxjs/create-crxjs/refs/heads/main${url}`,
-    { parseResponse: txt => txt },
-  )
-  if (data) {
-    loadedCode.value = data
+if (import.meta.client) {
+  try {
+    const data = await $fetch<string>(
+      `https://raw.githubusercontent.com/crxjs/create-crxjs/refs/heads/main${url}`,
+      { parseResponse: txt => txt },
+    )
+    if (data) {
+      loadedCode.value = data
+    }
   }
-}
-catch {
-  console.error(`Cannot load code: ${url}`)
+  catch {
+    console.error(`Cannot load code: ${url}`)
+  }
 }
 
 const md = `
@@ -28,18 +30,20 @@ ${loadedCode.value}
 </script>
 
 <template>
-  <MDC
-    v-if="loadedCode"
-    :value="md"
-    class="first:-mb-5"
-  />
-  <UiAlert
-    v-else
-    variant="destructive"
-  >
-    <UiAlertTitle>Error</UiAlertTitle>
-    <UiAlertDescription>
-      Cannot load code: <ProseCodeInline>{{ url }}</ProseCodeInline>
-    </UiAlertDescription>
-  </UiAlert>
+  <ClientOnly>
+    <MDC
+      v-if="loadedCode"
+      :value="md"
+      class="first:-mb-5"
+    />
+    <UiAlert
+      v-else
+      variant="destructive"
+    >
+      <UiAlertTitle>Error</UiAlertTitle>
+      <UiAlertDescription>
+        Cannot load code: <ProseCodeInline>{{ url }}</ProseCodeInline>
+      </UiAlertDescription>
+    </UiAlert>
+  </ClientOnly>
 </template>
